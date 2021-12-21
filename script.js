b644d1ea0ed4b0c95e6aee6327bd8be2a15b880d
Richard Rogers <richardrogers@Richards-MacBook-Pro.local>
2021-12-20 21:07:58 -0700

    linking spider-man comics through js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-var getComics = fetch("https://gateway.marvel.com:443/v1/public/characters/marvel/comics?apikey=be0e3fcf555a0c55bcc2dcf259693ba6c0f377c1")
+var offset=0
+var spideyCardEl=document.getElementById("spideyCard")
+var img404="http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available/portrait_uncanny.jpg"
+var getComic =()=>{
+  var timeStamp=Date.now()
+  console.log(timeStamp)
+  fetch("https://gateway.marvel.com/v1/public/characters?apikey=0b48117c76f758b006d4965cec8c77de&hash=5dcc745e0dd3368d15664b831a173cde&ts=1640054832677&limit=100&offset=1200")
   
     .then(function (response) {
         return response.json();
@@ -6,5 +12,36 @@ var getComics = fetch("https://gateway.marvel.com:443/v1/public/characters/marve
       .then(function (data) {
         console.log (data);
       });
-      
+}  
 
+function getParker(){
+  var id=1009610
+  fetch("https://gateway.marvel.com/v1/public/characters/"+id+"/comics?apikey=0b48117c76f758b006d4965cec8c77de&hash=5dcc745e0dd3368d15664b831a173cde&ts=1640054832677&limit=20&offset="+offset)
+  
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log (data);
+        for (var i=0; i<data.data.results.length; i++){
+          var aEl=document.createElement("a")
+          aEl.setAttribute("href", data.data.results[i].urls[0].url)
+          var imgEl=document.createElement("img")
+          var imgUrl=data.data.results[i].images[0]?.path
+          var imgExtension=data.data.results[i].images[0]?.extension
+          if (!imgUrl){
+            imgUrl=img404
+          } else {
+            imgUrl=imgUrl+"/portrait_uncanny."+imgExtension
+          }
+          console.log(imgUrl)
+          imgEl.setAttribute("src", imgUrl)
+          imgEl.setAttribute("alt", "work")
+          aEl.append(imgEl)
+          spideyCardEl.append(aEl)  
+        }
+        
+      });
+}
+
+getParker()
