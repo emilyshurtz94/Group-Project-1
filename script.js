9808707b85958f2343b969032eabf098a6d46684
Karrie Garzelloni <kgarzelloni@yahoo.com>
2021-12-23 15:36:25 -0700

    added map locations in index.html map style in CSS

--- script.js
+++ script.js
@@ -89,25 +89,3 @@ const currentPageEl=document.getElementById("current")
        getCharacter(id,newOffset,charName)
       
   })
-
-
-mapboxgl.accessToken = "pk.eyJ1Ijoia2dhcnplbGxvbmkiLCJhIjoiY2t4Z2ZiNGFzMXpubzJ2c3RmajgwdDhvbCJ9.TUr86IuVKdUCu6QQArj6aw";
-var map = new mapboxgl.Map({
-  container: 'map', // HTML container id
-  style: 'mapbox://styles/mapbox/streets-v9', // style URL
-  center: [-21.9270884, 64.1436456], // starting position as [lng, lat]
-  zoom: 13
-});
-var marker = new mapboxgl.Marker()
-.setLngLat([-21.9270884, 64.1436456])
-.addTo(map);
-
-
-mapboxgl.accessToken =
-"pk.eyJ1Ijoia2dhcnplbGxvbmkiLCJhIjoiY2t4Z2ZiNGFzMXpubzJ2c3RmajgwdDhvbCJ9.TUr86IuVKdUCu6QQArj6aw";
-const map = new mapboxgl.Map({
-container: 'map', // container ID
-style: 'mapbox://styles/mapbox/streets-v11', // style URL
-center: [-74.5, 40], // starting position [lng, lat]
-zoom: 9 // starting zoom
-});
